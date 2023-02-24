7bc5cd77f3c4165cc30c61332d73b283238f75fc
Armen Shadoyan <armenshadoyan2022@gmail.com>
2023-02-24 17:26:31 +0300

    connected  webpack

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  entry: '/src/js/index.js',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+    filename: 'bundle.js'
+  },
+  mode: 'development'
+};
